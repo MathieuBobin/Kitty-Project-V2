901a0dcf71a4621a05a7981e85824766e8e1a5d0
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-01 15:47:30 +0200

    backend cart

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,17 @@
 class ItemsController < ApplicationController
+  def index 
+    @items = Item.all 
+  end
+
+  def show
+    @items = Item.find(params[:id])
+  end
+
+  def new
+    @items = Item.new
+  end
+
+  
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_091046) do
+ActiveRecord::Schema.define(version: 2020_06_01_124222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["item_id"], name: "index_cart_items_on_item_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.boolean "ordered"
