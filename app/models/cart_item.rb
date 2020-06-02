fc9320546f35820ee3f5100ff36e78bbcbb33378
ouin85 <bouinten@yahoo.fr>
2020-06-02 19:25:21 +0200

    --

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
   # Validations
-  validates :cart_id, uniqueness: { scope: :item_id, message: ': ce preduit est dèjà présent dans ton panier' }
+  validates :cart_id, uniqueness: { scope: :item_id, message: ': Le produit a déjà été ajouté à votre panier' }
   
   belongs_to :cart 
   belongs_to :item
