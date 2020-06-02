01d28a158d2e2629efdb773b393b28fc26b141d3
Mathieu <bobin.mathieu.f@gmail.com>
2020-06-02 19:00:52 +0200

    modification flash

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,6 +1,6 @@
 class CartItem < ApplicationRecord
   # Validations
-  validates :cart_id, uniqueness: { scope: :item_id, message: ': item already exist in your cart' }
+  validates :cart_id, uniqueness: { scope: :item_id, message: ': Le produit a déjà été ajouté à votre panier' }
   
   belongs_to :cart 
   belongs_to :item
