a13ca90f0d6ae8f3b14332c7340ba39fda2767d2
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-03 22:07:28 +0200

    mail image after order

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,15 @@
 class Item < ApplicationRecord
+  include Rails.application.routes.url_helpers
+  
+  has_many_attached :images
   has_many :order_items
   has_many :orders, through: :order_items
   has_many :cart_items
   has_many :carts, through: :cart_items
   has_one_attached :image
+
+  def my_url
+    rails_blob_path(item.images, disposition: "attachment")
+  end
+
 end
