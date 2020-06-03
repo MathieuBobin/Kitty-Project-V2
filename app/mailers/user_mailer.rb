5e0fc9ddc45efe2403d85d63ab357793e0da46d8
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-03 10:10:58 +0200

    fix bug mails heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'zekittenproject@gmail.com'
  
   def welcome_email(user)
-    on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
+    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @user = user 
 
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
