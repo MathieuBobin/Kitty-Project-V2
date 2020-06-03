c55f8ad24340e6b5fbd3986b79b669981b811df5
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-03 11:19:57 +0200

    test mailers

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -5,12 +5,12 @@ class OrderMailer < ApplicationMailer
     #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
     @order = order 
     @user = order.user
-    @admin = 'kittenproject@yopmail.fr'    
+    #@admin = 'kittenproject@yopmail.fr'    
     #on définit une variable @url qu'on utilisera dans la view d’e-mail
     @url  = 'https://kitten-project-development.herokuapp.com/' 
 
     # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
     mail(to: @user.email, subject: 'Votre commande chez Ze Kitten Project!') 
-    mail(to: @admin, subject: 'Historique des commandes!') 
+    #mail(to: @admin, subject: 'Historique des commandes!') 
   end
 end
