8023086079d437a13fa3f414d06d7c9f9b2479cc
mathildy <mathilde.prouchandy@gmail.com>
2020-06-04 10:25:01 +0200

    timezone

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ Bundler.require(*Rails.groups)
 module KittenProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Europe/Paris'
+    config.active_record.default_timezone = :local
     config.load_defaults 5.2
     config.action_mailer.delivery_method = :mailjet
     # Settings in config/environments/* take precedence over those specified here.
