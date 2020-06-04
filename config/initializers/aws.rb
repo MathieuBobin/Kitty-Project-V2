b4dcbaedbf464c4f0bdc3b56a6be5e9de63be52e
TimotheRuffino <timothe.ruffino@gmail.com>
2020-06-04 12:36:07 +0200

    fix bugs heroku

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'us-east-1',
-  credentials: Aws::Credentials.new(ENV['AMAZON_ACCESS_KEY_ID'], ENV['AMAZON_SECRET_ACCESS_KEY']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET'])
