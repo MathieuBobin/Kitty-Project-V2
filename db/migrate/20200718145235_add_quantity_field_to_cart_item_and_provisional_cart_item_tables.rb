aa3c7783255693f048c1160365aa70e5ecc870ff
ouin85 <bouinten@yahoo.fr>
2020-07-19 19:21:26 +0200

    fix issue migration which add quantity field

--- db/migrate/20200718145235_add_quantity_field_to_cart_item_and_provisional_cart_item_tables.rb
+++ db/migrate/20200718145235_add_quantity_field_to_cart_item_and_provisional_cart_item_tables.rb
@@ -1,6 +1,11 @@
 class AddQuantityFieldToCartItemAndProvisionalCartItemTables < ActiveRecord::Migration[5.2]
-  def change
-    change_column :cart_items, :quantity, :integer, :default=> 1
+  def up
+    add_column :cart_items, :quantity, :integer, :default=> 1
     add_column :provisional_cart_items, :quantity, :integer, :default=>1
+  end
+
+  def down
+    remove_column :cart_items, :quantity
+    remove_column :provisional_cart_items, :quantity
   end
 end
